b93de9606ed8fb800da148b915b9217f22bea40c
jayrencalub <thejayren.calub@gmail.com>
2025-04-15 13:15:48 -0700

    experimenting with Character base class

---
+++ src/Character.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <iostream>
+
+// Include SFML libraries
+#include <SFML/Graphics.hpp>
+#include <SFML/Graphics/RectangleShape.hpp>
--- src/Machine.h
+++ src/Machine.h
@@ -4,7 +4,6 @@
 #include <iostream>
 
 // Include SFML libraries
-#include <SFML/Audio.hpp>
 #include <SFML/Graphics.hpp>
 #include <SFML/Graphics/RectangleShape.hpp>
 
