20b38b0569407248d2695c3bd00a9425bf931835
jayrencalub <thejayren.calub@gmail.com>
2025-04-17 11:42:35 -0700

    slight addition to Character class

--- src/Character.h
+++ src/Character.h
@@ -5,12 +5,13 @@
 // Include SFML libraries
 #include <SFML/Graphics.hpp>
 #include <SFML/Graphics/RectangleShape.hpp>
+#include <SFML/Graphics/Sprite.hpp>
 
-class Character : public sf::RectangleShape {
+class Character : public sf::RectangleShape { // RectangleShape or Sprite?
 protected:
 	double hp, damage, attackSpeed;
 	string ability;
 
 public:
-
+	
 };
