f1a862a94d8b3105805e22ea7f4937f8d0a29ea8
Mulroyp007 <mulroyp007@gmail.com>
2025-04-18 20:40:36 -0700

    Removed broken Constructors
    Need to wait for character class to have an actual texture variable to pass to the sprite constructor

--- src/towers/Barricade.h
+++ src/towers/Barricade.h
@@ -10,7 +10,7 @@
 
 class Barricade : public Tower
 {
-	Barricade() : Tower(sf::Texture(), 15, 1, { 0, 0 });
+	//Barricade() : Tower(sf::Texture(), 15, 1, { 0, 0 });
 
 	void useAbility();
 
--- src/towers/Stickman.h
+++ src/towers/Stickman.h
@@ -11,7 +11,7 @@
 
 class Stickman : public Tower
 {
-	Stickman() : Tower(sf::Texture(), 10, .5, { 0, 0 });
+	//Stickman() : Tower(sf::Texture(), 10, .5, { 0, 0 });
 
 	void useAbility();
 
