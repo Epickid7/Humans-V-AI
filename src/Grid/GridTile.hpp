7390d490856cfcea6a368ba699d0681141d671a0
Epickid7 <92333298+Epickid7@users.noreply.github.com>
2025-04-18 21:29:43 -0700

    hasTower member

--- src/Grid/GridTile.hpp
+++ src/Grid/GridTile.hpp
@@ -14,6 +14,7 @@ class GridTile : public sf::Sprite
 public:
 	GridTile(sf::Vector2f pos = {0.0,0.0}, sf::Vector2f square_width = { 0.0,0.0 }, const sf::Texture& texture = {}) : Sprite(texture) {
 		this->setPosition(pos);
+		this->hasTower = false;
 		
 	}
 
@@ -23,14 +24,17 @@ public:
 
 
 	void setSize(sf::Vector2f newSize);
+	void setHasTower(bool newHasTower);
 
 	sf::Vector2f getSize();
+	bool getHasTower();
 
 private:
 	//I need to determine how they will click on a specific square
 	//window width -offset % grid width (column number)
 	//window_heighth - offset (if any) % grid width (row number)
 	sf::Vector2f size;
+	bool hasTower;
 
 	
 
