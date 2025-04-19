d2e89d970f441da77286ca3174bedd11990bcfd2
jayrencalub <thejayren.calub@gmail.com>
2025-04-18 21:42:26 -0700

    added getters and setters

--- src/Character.cpp
+++ src/Character.cpp
@@ -6,37 +6,57 @@
 
 // getters
 int Character::getX() {
-
+	return location[0];
 }
 
 int Character::getY() {
-
+	return location[1];
 }
 double Character::getCurrentHealth() {
-
+	return currentHealth;
 }
 
 double Character::getMaxHealth() {
-
+	return maxHealth;
 }
 
 double Character::getAbilityPower() {
-
+	return abilityPower;
 }
 
 double Character::getAbilitySpeed() {
-
+	return abilitySpeed;
 }
 
-string Character::getAttackType() {
-
+int Character::getAttackType() {
+	return attackType;
 }
 
 // setters
-void setX(int newX);
-void setY(int newY);
-void setCurrentHealth(double newCur);
-void setMaxHealth(double newMax);
-void setAbilityPower(double newPower);
-void setAbilitySpeed(double newSpeed);
-void setAttackType(string newType);
+void Character::setX(int newX) {
+	location[0] = newX;
+}
+
+void Character::setY(int newY) {
+	location[1] = newY;
+}
+
+void Character::setCurrentHealth(double newCur) {
+	currentHealth = newCur;
+}
+
+void Character::setMaxHealth(double newMax) {
+	maxHealth = newMax;
+}
+
+void Character::setAbilityPower(double newPower) {
+	abilityPower = newPower;
+}
+
+void Character::setAbilitySpeed(double newSpeed) {
+	abilitySpeed = newSpeed;
+}
+
+void Character::setAttackType(AttackType newType) {
+	attackType = newType;
+}
