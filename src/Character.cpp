80bb2f13f344c65b416c9c770e1989d20883e473
Epickid7 <92333298+Epickid7@users.noreply.github.com>
2025-04-18 21:19:57 -0700

    return statements

--- src/Character.cpp
+++ src/Character.cpp
@@ -6,37 +6,58 @@
 
 // getters
 int Character::getX() {
-
+	return 0;
 }
 
 int Character::getY() {
-
+	return 0;
 }
 double Character::getCurrentHealth() {
-
+	return 0;
 }
 
 double Character::getMaxHealth() {
-
+	return 0;
 }
 
 double Character::getAbilityPower() {
-
+	return 0;
 }
 
 double Character::getAbilitySpeed() {
-
+	return 0;
 }
 
 string Character::getAttackType() {
+	return nullptr;
+}
 
+void Character::setX(int newX)
+{
+}
+
+void Character::setY(int newY)
+{
+}
+
+void Character::setCurrentHealth(double newCur)
+{
+}
+
+void Character::setMaxHealth(double newMax)
+{
+}
+
+void Character::setAbilityPower(double newPower)
+{
+}
+
+void Character::setAbilitySpeed(double newSpeed)
+{
+}
+
+void Character::setAttackType(string newType)
+{
 }
 
 // setters
-void setX(int newX);
-void setY(int newY);
-void setCurrentHealth(double newCur);
-void setMaxHealth(double newMax);
-void setAbilityPower(double newPower);
-void setAbilitySpeed(double newSpeed);
-void setAttackType(string newType);
