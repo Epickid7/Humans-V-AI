20403e3516e7b24c081f296e7bb933f17c82e2a9
Epickid7 <92333298+Epickid7@users.noreply.github.com>
2025-04-19 16:12:07 -0700

    Trying to fix sprite placement

--- main.cpp
+++ main.cpp
@@ -95,11 +95,22 @@ int main()
 
         if (sf::Mouse::isButtonPressed(sf::Mouse::Button::Left))
         {
+            for (int i = 0; i < ROW; i++) {
+                for (int j = 0; j < COLUMN; j++) {
+                    // we need overloaded assignment operator
+                    //declare with texture?
+                    if ((*gameBoard[i][j]).getGlobalBounds().contains(sf::Vector2f(sf::Mouse::getPosition().x,
+                        sf::Mouse::getPosition().y))) {
+                        sprite.setPosition(sf::Vector2f(sf::Mouse::getPosition().x, sf::Mouse::getPosition().y));
+                        sprite.setColor(sf::Color::Red);
+                    }
+                }
+            }
             //this gets the desktop coordinates of the mouse, not the window coords
-            sprite.setPosition(sf::Vector2f(sf::Mouse::getPosition(window).x, sf::Mouse::getPosition(window).y));
+           // sprite.setPosition(sf::Vector2f(sf::Mouse::getPosition(window).x, sf::Mouse::getPosition(window).y));
             
            
-            sprite.setColor(sf::Color::Red);
+           
            
         }
         
