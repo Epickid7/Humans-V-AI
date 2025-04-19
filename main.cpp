12886e7877d0bba2f10631427389db8fe5e6f0d0
Epickid7 <92333298+Epickid7@users.noreply.github.com>
2025-04-18 19:00:03 -0700

    Deleted image

--- main.cpp
+++ main.cpp
@@ -39,7 +39,9 @@ int main()
     //Machine m2(sf::Vector2f(window.getSize().x / 20, window.getSize().y / 10), sf::Vector2f(window.getSize().x + 100, window.getSize().y/2));
 
     //test texture
-    //sf::Texture t("assets/images/Jacob_Parnell_(LeKOBE)_the_kingdom_of_God_1ec41887-eaea-4609-922d-d2d0b6e5e0c9.png");
+    sf::Texture t;
+    t.loadFromFile("./Jacob_Parnell_(LeKOBE)_the_kingdom_of_God_1ec41887-eaea-4609-922d-d2d0b6e5e0c9.png");
+    // ("assets/images/Jacob_Parnell_(LeKOBE)_the_kingdom_of_God_1ec41887-eaea-4609-922d-d2d0b6e5e0c9.png");
     
     //Grid gameBoard;
 
