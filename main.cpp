9daebb6f600ae874e198e3218162a7f89811d39a
Epickid7 <92333298+Epickid7@users.noreply.github.com>
2025-04-18 19:54:34 -0700

    Drawing board

--- main.cpp
+++ main.cpp
@@ -39,8 +39,8 @@ int main()
     //Machine m2(sf::Vector2f(window.getSize().x / 20, window.getSize().y / 10), sf::Vector2f(window.getSize().x + 100, window.getSize().y/2));
 
     //test texture
-    sf::Texture t(ASSETS_PATH "/images/grass-pixel-art-background-grass-texture-pixel-art-vector-flower-garden-700-238894687.jpg");
-    sf::Sprite sprite(t);
+    sf::Texture grass(ASSETS_PATH "/images/grass-pixel-art-background-grass-texture-pixel-art-vector-flower-garden-700-238894687.jpg");
+    sf::Sprite sprite(grass);
     //t.loadFromFile(ASSETS_PATH "/images/8_bit_pixel_dragon_by_elpixelboy_d87udq0 - fullview.jpg");
     // ("assets/images/Jacob_Parnell_(LeKOBE)_the_kingdom_of_God_1ec41887-eaea-4609-922d-d2d0b6e5e0c9.png");
     
@@ -54,7 +54,8 @@ int main()
         for (int j = 0; j < 10; j++) {
             // we need overloaded assignment operator
             //declare with texture?
-           // gameBoard[i][j] = new GridTile({0.0,0.0},{window.getSize().x/10,window.getSize().x/10}, {});
+           gameBoard[i][j] = new GridTile(sf::Vector2f(i*window.getSize().x/10,j*window.getSize().y / 10 ),
+               sf::Vector2f(window.getSize().x / 10,window.getSize().x / 10), grass);
         }
     }
 
@@ -90,6 +91,13 @@ int main()
         //window.draw(text);
         window.draw(sprite);
         
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 10; j++) {
+                // we need overloaded assignment operator
+                //declare with texture?
+                window.draw(*gameBoard[i][j]);
+            }
+        }
 
         //Draw 'Machine'
         //window.draw(m);
