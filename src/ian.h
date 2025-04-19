c014136669dd0f15ed85a99d5683d59b5435fda8
Ian Van De Bogert <ian.vandebogert@wsu.edu>
2025-04-18 19:32:31 -0700

    Finished Constructor for Projectile

--- src/ian.h
+++ src/ian.h
@@ -1,12 +1,14 @@
 #pragma once
 
+#include <SFML/Graphics.hpp>
+
 class Projectile
 {
 public:
-	Projectile();
-	Projectile() {}
+	Projectile(const sf::Vector2f& size, const sf::Vector2f& pos, sf::Texture& texture, int newDamage, double newSpeed) { this->damage = newDamage, this->speed = newSpeed; }
 
 private:
 	int damage;
+	double speed;
 
 };
