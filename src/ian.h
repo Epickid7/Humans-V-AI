ed6d0c5cc11b2fecddebf2d5cf7aa73120b33fb7
Ian Van De Bogert <ian.vandebogert@wsu.edu>
2025-04-18 20:17:03 -0700

    Added more functions to Projectiles

--- src/ian.h
+++ src/ian.h
@@ -1,14 +1,36 @@
 #pragma once
 
 #include <SFML/Graphics.hpp>
+#include <iostream>
 
-class Projectile
+
+class Projectile : public sf::Sprite
 {
 public:
-	Projectile(const sf::Vector2f& size, const sf::Vector2f& pos, sf::Texture& texture, int newDamage, double newSpeed) { this->damage = newDamage, this->speed = newSpeed; }
+	// Projectile Constructor
+	Projectile(const sf::Vector2f& pos, const sf::Vector2f& direction, sf::Texture& texture, int newDamage, double newSpeed) : sf::Sprite(texture), speed(newSpeed), damage(newDamage), direction(direction) { setPosition(pos); }
+
+	// update position of the projectile in gameplay
+	void update_pos(double time)
+	{
+		sf::Vector2f change = direction * speed * time;
+		move(change); // move the projectile based on its direction, speed, and time in the air...
+	}
+
+	// damage getter
+	int getDamage(void) const
+	{
+		return damage;
+	}
+
+	bool OuterBounds() 
+	{
+		
+	}
+
 
 private:
 	int damage;
 	double speed;
-
+	sf::Vector2f direction;
 };
