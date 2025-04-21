b7f07cc487690c02303b23c742e6e1481fc8ee40
Mulroyp007 <mulroyp007@gmail.com>
2025-04-21 11:53:41 -0700

    Fixed the towers

--- src/Tower.cpp
+++ src/Tower.cpp
@@ -1,8 +1,8 @@
 #include "Tower.h"
 
 
-Tower::Tower(int x = 0, int y = 0, double current = 0, double max = 0, double power = 0, double speed = 0, string type = "",
-	const sf::Texture& texture = sf::Texture(ASSETS_PATH "/images/default.png"), int cost = 0)
+Tower::Tower(int x = 0, int y = 0, double current = 0, double max = 0, double power = 0, double speed = 0,
+	int type = AttackType::NEITHER, const sf::Texture& texture = sf::Texture(ASSETS_PATH "/images/default.png"), int cost = 0)
 	: Character(x, y, current, max, power, speed, type, texture)
 {
 	setCost(cost);
--- src/Tower.h
+++ src/Tower.h
@@ -13,9 +13,8 @@ class Tower : public Character
 
 public:
 
-	Tower(int x = 0, int y = 0, double current = 0, double max = 0, double power = 0, double speed = 0, string type = "",
-		const sf::Texture& texture = sf::Texture(ASSETS_PATH "/images/default.png"), int cost = 0) 
-		: Character(x, y, current, max, power, speed, type, texture);
+	Tower(int x, int y, double current, double max, double power, double speed,
+		int type, const sf::Texture& texture, int cost);
 
 	Tower(Tower &t);
 	~Tower();
