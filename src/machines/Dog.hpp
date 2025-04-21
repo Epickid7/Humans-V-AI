05272b853da6a884b858e82dbd66de1c9213c12b
jayrencalub <thejayren.calub@gmail.com>
2025-04-21 12:18:30 -0700

    added member functions for machines

--- src/machines/Dog.hpp
+++ src/machines/Dog.hpp
@@ -9,5 +9,19 @@ private:
 
 
 public:
+	Machine(int x = 0, int y = 0, double current = 0, double max = 0, double power = 0, double speed = 0,
+		int type = AttackType::NEITHER, const sf::Texture& texture = sf::Texture(ASSETS_PATH "/images/default.png"),
+		int newPayout = 0, double newDamage = 0, double newMoveSpeed = 0, bool inRange = false)
+		: Character(x, y, current, max, power, speed, type, texture) {}
 
+	void useAbility();
+	void move();
 };
+
+void Toaster::useAbility() {
+
+}
+
+void Toaster::move() {
+
+}
