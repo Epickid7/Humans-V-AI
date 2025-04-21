8d5279e948ca8b080b916c7b9156eef7161abd96
Mulroyp007 <mulroyp007@gmail.com>
2025-04-21 13:02:51 -0700

    Added Default Constructors

--- src/towers/People.cpp
+++ src/towers/People.cpp
@@ -1,4 +1,9 @@
 #include "People.h"
+#include "People.h"
+
+People::People() : Tower(0, 0, 10, 10, 2, 1, AttackType::PROJECTILE, sf::Texture(bankTexture), 60)
+{
+}
 
 People::People(int x, int y) : Tower(x, y, 10, 10, 2, 1, AttackType::PROJECTILE, sf::Texture(peopleTexture), 60)
 {
