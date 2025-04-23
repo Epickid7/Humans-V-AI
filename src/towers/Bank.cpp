7549b4d5652d0f53ad11c676e9d4f4b07c81dc9b
Mulroyp007 <mulroyp007@gmail.com>
2025-04-23 12:04:13 -0700

    Added default constructor definition

--- src/towers/Bank.cpp
+++ src/towers/Bank.cpp
@@ -1,5 +1,9 @@
 #include "Bank.h"
 
+Bank::Bank() : Tower(0, 0, 10, 10, 20, 1, AttackType::NEITHER, sf::Texture(bankTexture), 50)
+{
+}
+
 Bank::Bank(int x, int y) : Tower(x, y, 10, 10, 20, 1, AttackType::NEITHER, sf::Texture(bankTexture), 50)
 {
 
