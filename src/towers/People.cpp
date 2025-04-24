6efb53e8ace18f1a307ef72169b132792e26a8a8
Epickid7 <92333298+Epickid7@users.noreply.github.com>
2025-04-23 20:57:42 -0700

    serious progress

--- src/towers/People.cpp
+++ src/towers/People.cpp
@@ -1,13 +1,13 @@
 #include "People.h"
 
 
-People::People() : Tower(0, 0, 10, 10, 2, 1, AttackType::PROJECTILE, peopleTexture, 60)
+People::People() : Tower(0, 0, 10, 10, 2, 1, AttackType::PROJECTILE, sf::Texture(ASSETS_PATH "/images/people.png"), 60)
 {
 	peopleTexture.loadFromFile(ASSETS_PATH "/images/people.png");
 	brickTexture.loadFromFile(ASSETS_PATH "/images/brick.png");
 }
 
-People::People(int x, int y) : Tower(x, y, 10, 10, 2, 1, AttackType::PROJECTILE, peopleTexture, 60)
+People::People(int x, int y) : Tower(x, y, 10, 10, 2, 1, AttackType::PROJECTILE, sf::Texture(ASSETS_PATH "/images/people.png"), 60)
 {
 	peopleTexture.loadFromFile(ASSETS_PATH "/images/people.png");
 	brickTexture.loadFromFile(ASSETS_PATH "/images/brick.png");
