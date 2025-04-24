0f35907bea756ed666f792f3c5f675dab1f21aa3
Epickid7 <92333298+Epickid7@users.noreply.github.com>
2025-04-23 21:10:12 -0700

    creating people (texture issue)

--- src/Tower.h
+++ src/Tower.h
@@ -15,7 +15,7 @@ class Tower : public Character
 public:
 
 	Tower::Tower(int x = 0, int y = 0, double current = 0, double max = 0, double power = 0, double speed = 0,
-		int type = AttackType::NEITHER, const sf::Texture& texture = sf::Texture(ASSETS_PATH "/images/default.png"), int cost = 0)
+		int type = AttackType::NEITHER, sf::Texture& texture = sf::Texture(ASSETS_PATH "/images/default.png"), int cost = 0)
 		: Character(x, y, current, max, power, speed, type, texture)
 	{
 		setCost(cost);
